7dee664f53bb40e416ca4572d9d45c608d8ae15a
snrntpa2z@naver.com <snrntpa2z@naver.com>
2019-03-24 14:03:36 +0900

    3/24 : package.json 수정, Main 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import apollo from "./apolloClient";
 import { ApolloProvider } from "react-apollo";
+import Main from "pages/Main";
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={apollo}>
         <Router>
-          <div className="App" />
+          <div className="App">
+            <Route exact path="/" component={Main} />
+          </div>
         </Router>
       </ApolloProvider>
     );
---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div id="main">
+        <header>header</header>
+        <section>Section</section>
+        <footer>footer</footer>
+      </div>
+    );
+  }
+}

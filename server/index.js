81bac456325603a5174af51b1e35c61f08766aa6
snrntpa2z@naver.com <snrntpa2z@naver.com>
2019-03-24 13:52:34 +0900

    3/24 : server folder 추가

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
+import mongoose from "mongoose";
+
+const start = async () => {
+  await mongoose.connect(
+    "mongodb://owner:ownertest1@ds123896.mlab.com:23896/owner"
+  );
+  const book = mongoose.model("book", { title: String, author: String });
+  const user = mongoose.model("user", {
+    id: String,
+    pwd: String,
+    name: String
+  });
+  const server = new GraphQLServer({
+    typeDefs: "server/graphql/schema.graphql",
+    resolvers,
+    context: { book, user }
+  });
+  server.start(() => console.log("GraphQL Server Running"));
+};
+start();
